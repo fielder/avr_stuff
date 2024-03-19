ad660f450f91fb59fe8dfafbfb00cffc18866817
Chris Fielder <shambler.qc@gmail.com>
2024-03-18 23:58:24 -0500

    Partial PWM working

--- attiny85/pwm/pwm.c
+++ attiny85/pwm/pwm.c
@@ -45,18 +45,19 @@ main (void)
 
 // Pin 3 - PB4 - OC1B - Timer/Counter1 Compare Match B Output
 
-	//OCR1A = 0;
-	//OCR1B = 127; // this is the one want for OC1B, our output pin
-	OCR1C = 199; // resetting/max value for counter?
+	//OCR1A = 199;
+	OCR1B = 199;
+	//OCR1C = 199;
 
 	// OC1B cleared on compare match; inverted disabled
 	GTCCR = (1 << PWM1B) | (0 << COM1B1) | (1 << COM1B0);
-	TCCR1 = (0 << COM1A1) | (1 << COM1A0);
+	TCCR1 = (1 << CS13) | (0 << CS12) | (1 << CS11) | (1 << CS10);
+	//TIMSK = 0;
 
 	// Fast PWM; 0xff TOP; update OCRx at BOTTOM; TOV flag set on MAX
 	//TCCR0A = (1 << WGM01) | (1 << WGM00);
 	// clk/1024
-	TCCR0B = (0 << WGM02) | (1 << CS02) | (0 << CS01) | (1 << CS00);
+	//TCCR0B = (0 << WGM02) | (1 << CS02) | (0 << CS01) | (1 << CS00);
 
 	sei ();
 
@@ -69,7 +70,7 @@ main (void)
 			PORTB &= ~_BV(PB2);
 			PORTB &= ~_BV(PB3);
 			is_on = 0;
-			//_delay_ms (1000);
+			_delay_ms (1000);
 		}
 		else
 		{
@@ -77,8 +78,7 @@ main (void)
 			PORTB |= _BV(PB2);
 			PORTB |= _BV(PB3);
 			is_on = 1;
-			//_delay_ms (1000);
+			_delay_ms (1000);
 		}
 	}
 }
-
