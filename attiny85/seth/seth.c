7ed8f8fa1cbc8f50883f01cb259a14cf42e653d0
Chris Fielder <shambler.qc@gmail.com>
2024-03-20 19:20:31 -0500

    Use a button input to verify PWM duty cycle is variable on the fly

--- attiny85/seth/seth.c
+++ attiny85/seth/seth.c
@@ -11,10 +11,10 @@
 int
 main (void)
 {
-	uint8_t is_on = 0;
+	uint8_t button_pressed = 0;
 
-	DDRB = _BV(PB4) | _BV(PB3) | _BV(PB2) | _BV(PB1); /* set outputs */
-	PORTB = 0x0;
+	DDRB = _BV(PB4) | _BV(PB3) | _BV(PB1); /* set outputs */
+	PORTB = _BV(PB2);
 
 // Fan PWM frequency: 21kHz - 28kHz (25kHz nominal)
 
@@ -45,49 +45,61 @@ main (void)
 
 // Pin 3 - PB4 - OC1B - Timer/Counter1 Compare Match B Output
 
+#define PWM_MAX_VAL 157
 	//OCR1A = 199; whas dis?
 	OCR1B = 79; // duty cycle: 0 to OCR1C
-	OCR1C = 157;
+	OCR1C = PWM_MAX_VAL;
 
 	// OC1B cleared on compare match; inverted not connected
 	GTCCR = (1 << PWM1B) | (1 << COM1B1) | (0 << COM1B0);
 
 	TCCR1 = (0 << CS13) | (0 << CS12) | (1 << CS11) | (0 << CS10);
 
+#if 0
 	// disable digital input buffer on ADC input
 	DIDR0 = (1 << ADC1D);
-	ADMUX = (0 << REFS2) | (0 << REFS1) | (1 << REFS0) |
-		(0 << MUX3) | (0 << MUX2) | (0 << MUX1) | (1 << MUX0) |
-		(1 << ADLAR);
 
-	ADCSRA = (1 << ADEN) |
-		(0 << ADPS2) | (0 << ADPS1) | (0 << ADPS0);
+	ADMUX = (0 << REFS2) | (0 << REFS1) | (1 << REFS0) | // AREF voltage reference
+		(0 << MUX3) | (0 << MUX2) | (0 << MUX1) | (1 << MUX0) | // ADC1 (PB2)
+		(1 << ADLAR); // put top 8 bits in 1 register for simpler reading (discard bottom 2 bits)
+
+	ADCSRA = (1 << ADEN) | // enable
+		(1 << ADPS2) | (1 << ADPS1) | (0 << ADPS0); // ADC scalar: 64
+#endif
 
 	sei ();
 
 	_delay_ms (50);
 	while (1)
 	{
-		ADCSRA |= (1 << ADSC);         // start ADC measurement
-		while (ADCSRA & (1 << ADSC) );
-		// use ADCH for value
-		// TODO: set pwm duty cycle in OCR1B
+#if 0
+		ADCSRA |= (1 << ADSC); // start ADC measurement
+		while (ADCSRA & (1 << ADSC)) {}
 
-		if (is_on)
+		uint8_t adch = ADCH;
+		float adc_percentage = adch / 255.0f;
+
+		float pwmout = adc_percentage * (float)PWM_MAX_VAL;
+		if (pwmout < 1)
+			pwmout = 0;
+		if (pwmout > PWM_MAX_VAL)
+			pwmout = PWM_MAX_VAL;
+		OCR1B = (uint8_t)pwmout;
+#endif
+
+		button_pressed = (PINB & _BV(PB2)) == 0x0;
+
+		if (!button_pressed)
 		{
 			PORTB &= ~_BV(PB1);
-			PORTB &= ~_BV(PB2);
 			PORTB &= ~_BV(PB3);
-			is_on = 0;
-			_delay_ms (100);
+			OCR1B = 1;
 		}
 		else
 		{
 			PORTB |= _BV(PB1);
-			PORTB |= _BV(PB2);
 			PORTB |= _BV(PB3);
-			is_on = 1;
-			_delay_ms (100);
+			OCR1B = PWM_MAX_VAL - 1;
 		}
 	}
 }
