f8da7ecead83307a09855c873dceaaca124a98fc
Chris Fielder <shambler.qc@gmail.com>
2026-02-09 19:31:54 -0600

    Pull the input byte from port B; active low Update a few makefiles

--- attiny48/7seg_1byte/main.c
+++ attiny48/7seg_1byte/main.c
@@ -7,6 +7,8 @@
 #include <util/delay.h>
 
 /*
+ * 2-digit 7-segment LED display pinout
+ *
 	0    1    2    3    4
 	.    .    .    .    .
 
@@ -23,8 +25,22 @@
 	7 - bl
 	8 - b
 	9 - mid
+
+	 ___0___
+	|       |
+	4       1
+	|___9___|
+	|       |
+	7       5
+	|___8___|
+
+		dec 6
 */
 
+/* map to port D outputs
+ * note this doesn't map directly to the display pins as LED pin 2 and 3
+ * are cathodes
+ */
 uint8_t LED_MAP[16][8] = {
 /*	 0  1  2  3  4  5  6  7 */
 	{1, 1, 1, 1, 0, 1, 1, 0}, // 0
@@ -88,69 +104,81 @@ SetOutputLED (uint8_t led_idx, uint8_t onoff)
 }
 
 
-static void
-setup (void)
+int
+main (void)
 {
 	DDRC = 0xff; /* 8 outputs */
 	PORTC = 0x0; /* start low */
 
 	/* input; latch; active low */
+// TODO: select latch pin; solder it up
 //	DDRC &= ~_BV(PC0);
 //	PORTC |= _BV(PC0); /* enable pull-up */
 
 	DDRD = 0xff; /* 8 outputs */
 	PORTD = 0x0; /* start low */
 
-	/* data byte */
+	/* input data byte */
 	DDRB = 0x00; /* 8 inputs */
 	PORTB = 0xff; /* enable pull-ups */
-}
-
-
-int
-main (void)
-{
-	setup ();
 
 	_delay_ms (50);
 
-	if (1)
+	uint32_t clk = 0;
+	uint8_t val = 0;
+
+	/* start off w/ low nibble selected */
+	PORTC |= _BV(PC4);
+
+	for (uint8_t i = 0; i < 8; i++)
+		SetOutputLED (i, 0);
+
+#define USE_INPUTS 1
+	while (1)
 	{
-		uint32_t clk = 0;
-		uint8_t val = 0;
+		if (USE_INPUTS) {
+			uint8_t latch_is_low = 0;
 
-		/* start off w/ low nibble selected */
-		PORTC |= _BV(PC4);
+			if (1) {
+				// TODO: read latch pin to trigger a read; active low
+				latch_is_low = 1;
+			}
 
-		for (uint8_t i = 0; i < 8; i++)
-			SetOutputLED (i, 0);
+			if (latch_is_low)
+			{
+				uint8_t in[8];
+				const uint8_t pin = PINB;
+				in[0] = (pin & _BV(PB0)) != 0x0;
+				in[1] = (pin & _BV(PB1)) != 0x0;
+				in[2] = (pin & _BV(PB2)) != 0x0;
+				in[3] = (pin & _BV(PB3)) != 0x0;
+				in[4] = (pin & _BV(PB4)) != 0x0;
+				in[5] = (pin & _BV(PB5)) != 0x0;
+				in[6] = (pin & _BV(PB6)) != 0x0;
+				in[7] = (pin & _BV(PB7)) != 0x0;
+				val = 0x0;
+				for (uint8_t i = 0; i < 8; i++)
+					val |= in[i] << i;
+			}
+		}
 
-		while (1)
-		{
-			PORTC &= ~_BV(PC5);
-			PORTC |= _BV(PC4); /* select low nibble */
-			for (uint8_t ledidx = 0; ledidx < 8; ledidx++)
-				SetOutputLED (ledidx, LED_MAP[(val >> 0) & 0xf][ledidx]);
-			_delay_ms (1); /* give a bit for the LEDs to show */
+		PORTC &= ~_BV(PC5);
+		PORTC |= _BV(PC4); /* select low nibble */
+		for (uint8_t ledidx = 0; ledidx < 8; ledidx++)
+			SetOutputLED (ledidx, LED_MAP[(val >> 0) & 0xf][ledidx]);
+		_delay_ms (1); /* give a bit for the LEDs to show */
 
-			PORTC |= _BV(PC5); /* select high nibble */
-			PORTC &= ~_BV(PC4);
-			for (uint8_t ledidx = 0; ledidx < 8; ledidx++)
-				SetOutputLED (ledidx, LED_MAP[(val >> 4) & 0xf][ledidx]);
-			_delay_ms (1); /* give a bit for the LEDs to show */
+		PORTC |= _BV(PC5); /* select high nibble */
+		PORTC &= ~_BV(PC4);
+		for (uint8_t ledidx = 0; ledidx < 8; ledidx++)
+			SetOutputLED (ledidx, LED_MAP[(val >> 4) & 0xf][ledidx]);
+		_delay_ms (1); /* give a bit for the LEDs to show */
 
-			clk += 1;
+		clk += 1;
+		if (!USE_INPUTS) {
 			if ((clk & 0xff) == 0x0)
 				val += 1;
 		}
 	}
-	else
-	{
-		// non-debug stuff
-		while (1)
-		{
-		}
-	}
 	return 0;
 }
-
